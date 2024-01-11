e3c410949ae47c641e2cf082516605fce97c24b1
lijian <lijian-736@163.com>
2024-01-11 10:44:49 +0800

    Change hello world demo print info

--- source/test_tvm_01_hello_world.cpp
+++ source/test_tvm_01_hello_world.cpp
@@ -11,12 +11,12 @@ using namespace tvm;
 using namespace tvm::runtime;
 
 //Register your global function
-TVM_REGISTER_GLOBAL("Chiplite-Print").set_body([](TVMArgs args, TVMRetValue* rv) {
-    std::cout << "Hello chiplite, welcome !" << std::endl;
+TVM_REGISTER_GLOBAL("TVM-Print").set_body([](TVMArgs args, TVMRetValue* rv) {
+    std::cout << "Hello TVM, welcome !" << std::endl;
 });
 
 int main(int argc, char** argv) {
-    const std::string func_name = "Chiplite-Print";
+    const std::string func_name = "TVM-Print";
     const tvm::runtime::PackedFunc* fp = tvm::runtime::Registry::Get(func_name);
     if (fp) {
         (*fp)();
