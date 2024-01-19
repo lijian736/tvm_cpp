198c185a39efe320b731d0c2b8016a2c71cd2f29
lijian <lijian-736@163.com>
2024-01-19 11:31:52 +0800

    Optimize reshape op

--- source/onnx_op/ops/reshape.cpp
+++ source/onnx_op/ops/reshape.cpp
@@ -26,15 +26,51 @@ Status ReshapeParser::parse_op(const onnx::NodeProto& proto_node,
 
     // get the inputs
     int input_size = proto_node.input_size();
-    for (int i = 0; i < input_size; ++i) {
-        const auto& input = proto_node.input(i);
+    if (input_size != 2) {
+        std::ostringstream oss;
+        oss << "Invalid inputs of Reshape: " << proto_node.name();
+        return Status(StatusCode::INVALID_MODEL, oss.str());
     }
 
     // get the outputs
     int output_size = proto_node.output_size();
-    for (int i = 0; i < output_size; ++i) {
-        const auto& output = proto_node.output(i);
+    if (output_size != 1) {
+        std::ostringstream oss;
+        oss << "Invalid outputs of Reshape: " << proto_node.name();
+        return Status(StatusCode::INVALID_MODEL, oss.str());
     }
+
+    const std::string& input = proto_node.input(0);
+    const std::string& new_shape = proto_node.input(1);
+    const std::string& output = proto_node.output(0);
+
+    auto input_iter = expressions.find(input);
+    if (input_iter == expressions.end()) {
+        std::ostringstream oss;
+        oss << "Input not found, Reshape: " << proto_node.name() << " input: " << input;
+        return Status(StatusCode::INVALID_MODEL, oss.str());
+    }
+
+    auto new_shape_iter = expressions.find(new_shape);
+    if (new_shape_iter == expressions.end()) {
+        std::ostringstream oss;
+        oss << "Input not found, Reshape: " << proto_node.name() << " input: " << new_shape;
+        return Status(StatusCode::INVALID_MODEL, oss.str());
+    }
+
+    tvm::relay::Expr result_expr = (*reshape)(input_iter->second, new_shape_iter->second, (allowzero ? true : false));
+
+    auto status = fold_const(result_expr);
+    if (!status.is_ok()) {
+        return status;
+    }
+
+    // add to expressions
+    auto ret = expressions.emplace(output, result_expr);
+    if (!ret.second) {
+        ret.first->second = result_expr;
+    }
+    relay = result_expr;
 
     return Status::ok();
 }
