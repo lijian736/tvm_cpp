25d5840831c8577f8cee7b2c044c7b54a7856573
lijian <lijian-736@163.com>
2024-01-20 22:27:25 +0800

    Add examples folder

---
+++ examples/example_01.cpp
@@ -0,0 +1,21 @@
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/text_format.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "onnx.proto3.pb.h"
+#include "utils/onnx_utils.h"
+#include "utils/utils.h"
+
+using namespace tvm_cpp::utils;
+using namespace tvm_cpp::onnx_utils;
+
+int main(int argc, char** argv) {
+    std::cout << "example_01" << std::endl;
+
+    return 0;
+}
