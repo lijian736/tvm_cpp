32b8b16f31805e5fd97ddb817c5f1ef15a43d5ef
lijian <lijian-736@163.com>
2024-01-25 00:02:57 +0800

    Optimize pass example 01

--- source/test_tvm_pass_01.cpp
+++ source/test_tvm_pass_01.cpp
@@ -4,6 +4,7 @@
 #include <tvm/runtime/memory.h>
 #include <tvm/runtime/object.h>
 #include <tvm/runtime/registry.h>
+#include <tvm/support/with.h>
 
 #include <algorithm>
 #include <iostream>
@@ -38,19 +39,23 @@ int main(int argc, char** argv) {
     }
 
     // generate the pass context
+
+    // the optimizataion level
     int opt_level = 1;
+    // the list of required passes
     tvm::runtime::Array<tvm::String> required;
+    // the list of disabled passes
     tvm::runtime::Array<tvm::String> disabled;
+    // a list of pass instrument implementations
     tvm::runtime::Array<tvm::instrument::PassInstrument> instruments;
+    // pass specific configurations
     tvm::runtime::Optional<Map<String, ObjectRef>> configs;
 
     PassContext pass_ctx = (*pass_ctx_gen)(opt_level, required, disabled, instruments, configs);
 
-    // enter the pass context
-    (*enter_pass_ctx)(pass_ctx);
+    tvm::With<PassContext> scope(pass_ctx);
 
-    // exit the pass context
-    (*exit_pass_ctx)(pass_ctx);
+    
 
     return 0;
 }
