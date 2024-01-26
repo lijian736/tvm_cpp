b5340ce9b1ebceb0b167e62c382c255587436cc2
lijian <lijian-736@163.com>
2024-01-26 11:03:05 +0800

    Add dead code elimination codes

--- source/test_tvm_pass_03_dead_code_elimination.cpp
+++ source/test_tvm_pass_03_dead_code_elimination.cpp
@@ -10,13 +10,17 @@
 #include "onnx.proto3.pb.h"
 #include "onnx_op/op_parser.h"
 #include "utils/onnx_utils.h"
+#include "utils/relay_generator.h"
 #include "utils/relay_utils.h"
+#include "utils/status.h"
 #include "utils/utils.h"
 
+using namespace tvm_cpp::common;
 using namespace tvm_cpp::utils;
 using namespace tvm_cpp::onnx_utils;
 using namespace tvm_cpp::onnx_op;
 using namespace tvm_cpp::relay_utils;
+using namespace tvm_cpp::relay_generator;
 
 using namespace tvm;
 using namespace tvm::relay;
@@ -24,46 +28,9 @@ using namespace tvm::relay::transform;
 using namespace tvm::runtime;
 
 int main(int argc, char** argv) {
-    OnnxOpParserRegister::get_instance()->register_all_supported_ops();
-
-    if (argc <= 1) {
-        std::cerr << "Invalid arguments" << std::endl;
-        return -1;
-    }
-
-    // Get input onnx file path
-    std::string file_name(argv[1]);
-    std::cout << "The input file name is: " << file_name << std::endl << std::endl;
-
-    // Check if the input file exist
-    trim(file_name);
-    bool exist = file_exist(file_name);
-    if (!exist) {
-        std::cerr << "The input file does NOT exist, please check the file path" << std::endl;
-        return -2;
-    }
-
-    // Read onnx file
-    std::ifstream ifs(file_name, std::ios::in | std::ios::binary);
-    if (!ifs.is_open()) {
-        std::cerr << "Open file failed: " << file_name << std::endl;
-        return -3;
-    }
-
-    onnx::ModelProto onnx_model;
-
-    google::protobuf::io::IstreamInputStream input_stream(&ifs);
-    google::protobuf::io::CodedInputStream coded_input(&input_stream);
-    bool parsed = onnx_model.ParseFromCodedStream(&coded_input);
-    ifs.close();
-    if (!parsed) {
-        std::cerr << "Parse onnx model failed: " << file_name << std::endl;
-        return -4;
-    }
-
-    // parse onnx file to IRModule
+        // generate module with dead codes
     tvm::IRModule module;
-    auto ret = parse_graph_to_irmodule(onnx_model.graph(), module);
+    auto ret = generate_dead_code_module(module);
     if (!ret.is_ok()) {
         std::cout << ret << std::endl;
         return -1;
@@ -81,7 +48,6 @@ int main(int argc, char** argv) {
     std::string before_str = (std::string)before_text;
     std::cout << "the ONNX relay IR Model info(before dead code elimination): " << std::endl << before_str << std::endl;
 
-    std::cout << "---------------------------------------------------" << std::endl;
     // print the new IR module after dead code elimination
     tvm::String after_text = (*pretty_print)(module);
     std::string after_str = (std::string)after_text;
---
+++ source/utils/relay_generator.h
@@ -0,0 +1,31 @@
+
+#ifndef _H_TVM_CPP_UTILS_RELAY_GENERATOR_H_
+#define _H_TVM_CPP_UTILS_RELAY_GENERATOR_H_
+
+#include <tvm/ir/module.h>
+#include <tvm/relay/expr.h>
+#include <tvm/relay/transform.h>
+#include <tvm/runtime/memory.h>
+#include <tvm/runtime/object.h>
+#include <tvm/runtime/registry.h>
+
+#include <string>
+#include <unordered_map>
+
+#include "status.h"
+
+namespace tvm_cpp {
+namespace relay_generator {
+
+/**
+ * @brief Generate an IRModule with dead codes
+ *
+ * @param module output parameter. the generated IRModule with dead codes
+ * @return Status
+ */
+Status generate_dead_code_module(tvm::IRModule& module);
+
+}    // namespace relay_generator
+}    // namespace tvm_cpp
+
+#endif
